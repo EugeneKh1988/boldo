c94dee3620b306af40c3292803b7fcd2d5977c6a
EugeneKh1988 <khiznyak.1988@gmail.com>
2022-08-16 16:40:57 +0300

    Added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       colors: {
         lightblue: '#0DBBFC',
-        green: '#65E4A3',
+        themegreen: '#65E4A3',
         lightgrey: '#F1F1F1',
         darkblue: '#0A2640',
         greyblue: '#8FB6D5',
